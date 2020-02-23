d4dc0972f2a347d1a5cfa405fa7aa96852806a08
Mari Ashley <andrey.korchmarek@yandex.ru>
2020-02-23 19:44:10 +0300

    19:44

--- test/test1.sh
+++ test/test1.sh
@@ -3,5 +3,14 @@
 imyaVM="imyaVM"
 
 VBoxManage unregistervm $imyaVM --delete
-VBoxManage createvm --name $imyaVM --register --ostype Debian_64 --basefolder /Volumes/Storage/goinfre/mashley/
+VBoxManage createvm --name $imyaVM \
+  --register \
+  --ostype Debian_64 \
+  --basefolder /Volumes/Storage/goinfre/mashley/
 #VBoxManage modifyvm $imyaVM
+VBoxManage modifyvm $imyaVM \
+  --memory 1024 \
+  --nic1 nat \
+  --nic2 bridged \
+  --boot1 disk
+#VBoxManage startvm imyaVM
